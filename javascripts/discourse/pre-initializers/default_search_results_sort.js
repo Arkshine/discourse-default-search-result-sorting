b909d0909306a003bfe3d3218c1fd1cc980d3fb8
Arkshine <github@arkshine.me>
2024-02-09 20:37:41 +0100

    DEV: Simplify code to match core changes and remove the settings

--- javascripts/discourse/pre-initializers/default_search_results_sort.js
+++ javascripts/discourse/pre-initializers/default_search_results_sort.js
@@ -1,12 +1,4 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { isValidSearchTerm } from "discourse/lib/search";
-import { scheduleOnce } from "@ember/runloop";
-import {
-  CATEGORY_SLUG_REGEXP,
-  DEFAULT_TYPE_FILTER,
-  SUGGESTIONS_REGEXP,
-  USERNAME_REGEXP,
-} from "discourse/widgets/search-menu";
 
 const PLUGIN_ID = "default-search-results-sorting";
 
@@ -15,73 +7,6 @@ export default {
   before: "inject-discourse-objects",
 
   initializeWithApi(api) {
-    /**
-     * Must be the same order as Discourse (drop-down list on full search page),
-     * and default_search_results_sorting_by setting.
-     */
-    const sortingChoices = {
-      relevance: undefined,
-      latest_post: "order:latest",
-      most_liked: "order:likes",
-      most_viewed: "order:views",
-      latest_topic: "order:latest_topic",
-      most_votes: "order:votes",
-    };
-
-    const defaultSorting =
-      sortingChoices[settings.default_search_results_sorting_by];
-
-    const defaultSortingIndex =
-      Object.values(sortingChoices).indexOf(defaultSorting);
-
-    if (defaultSorting === undefined) {
-      return;
-    }
-
-    function cleanTerm(term) {
-      return term.replace(defaultSorting, "").trim();
-    }
-
-    function cleanRecentSearches(currentUser) {
-      if (currentUser) {
-        let recentSearches = Object.assign(currentUser.recent_searches || []);
-        recentSearches = recentSearches
-          .map((term) => cleanTerm(term))
-          .filter((term) => term);
-
-        currentUser.set("recent_searches", recentSearches);
-      }
-    }
-
-    function includesTopics(searchData) {
-      return searchData.typeFilter !== DEFAULT_TYPE_FILTER;
-    }
-
-    const matchesSuggestions = (searchData) => {
-      const term = searchData.term.trim();
-
-      if (term === undefined || includesTopics(searchData)) {
-        return false;
-      }
-
-      const categoriesMatch = term.match(CATEGORY_SLUG_REGEXP);
-      if (categoriesMatch) {
-        return { type: "category", categoriesMatch };
-      }
-
-      const usernamesMatch = term.match(USERNAME_REGEXP);
-      if (usernamesMatch) {
-        return { type: "username", usernamesMatch };
-      }
-
-      const suggestionsMatch = term.match(SUGGESTIONS_REGEXP);
-      if (suggestionsMatch) {
-        return suggestionsMatch;
-      }
-
-      return false;
-    };
-
     /**
      * Full page search
      * -
@@ -99,100 +24,29 @@ export default {
     /**
      * Search menu
      * -
-     * Hides the sort order option from the search results.
+     * Changes the default sorting.
      */
-    api.reopenWidget("search-menu-results", {
+    api.modifyClass("component:search-menu/results/types", {
       pluginId: PLUGIN_ID,
 
-      html(attrs) {
-        const {
-          invalidTerm,
-          inPMInboxContext,
-          term,
-          suggestionKeyword,
-          noResults,
-          searchTopics,
-        } = attrs;
+      get filteredResultTypes() {
+        // return only topic result types
+        if (this.args.topicResultsOnly) {
+          const topics = this.args.resultTypes.filter(
+            (resultType) => resultType.type === "topic"
+          );
 
-        if (
-          suggestionKeyword !== false &&
-          suggestionKeyword?.includes(defaultSorting)
-        ) {
-          attrs.suggestionKeyword = cleanTerm(suggestionKeyword);
+          topics.firstObject.results.sort((a, b) => {
+            return b.created_at > a.created_at ? 1 : -1;
+          });
+
+          return topics;
         }
 
-        if (
-          !(searchTopics && (invalidTerm || noResults)) ||
-          ((!term || !searchTopics) && !inPMInboxContext)
-        ) {
-          cleanRecentSearches(this.currentUser);
-        }
-
-        if (term?.includes(defaultSorting)) {
-          attrs.term = cleanTerm(term);
-        }
-
-        return this._super(attrs);
-      },
-    });
-
-    /**
-     * Search menu
-     * -
-     * Adds the default sorting to the search term.
-     * Hides the sorting option from the recent searches.
-     */
-    api.reopenWidget("search-menu", {
-      pluginId: PLUGIN_ID,
-
-      /**
-       * Search menu
-       * -
-       * Adds the default sorting to the search term.
-       */
-      triggerSearch() {
-        if (
-          !this.searchData.term ||
-          !isValidSearchTerm(this.searchData.term, this.siteSettings)
-        ) {
-          this._super(...arguments);
-          return;
-        }
-
-        if (!this.searchData.term.includes(defaultSorting)) {
-          const matchSuggestions = matchesSuggestions(this.searchData);
-
-          if (
-            !matchSuggestions ||
-            !this.searchData.term.includes("order:") ||
-            (matchSuggestions.type !== "category" &&
-              matchSuggestions.type !== "username")
-          ) {
-            this.searchData.term += ` ${defaultSorting}`;
-          }
-        }
-
-        this._super(...arguments);
-      },
-
-      /**
-       * Search menu
-       * -
-       * Cleans input. The next time the user opens the search menu,
-       * the sorting option is added to the search term in some situations.
-       */
-      html(attrs, state) {
-        const content = this._super(attrs, state);
-
-        scheduleOnce("afterRender", () => {
-          const searchInput = document.getElementById("search-term");
-
-          if (searchInput?.value.includes(defaultSorting)) {
-            searchInput.value = cleanTerm(searchInput.value);
-          }
-        });
-
-        return content;
+        // return all result types minus topics
+        return this.args.resultTypes.filter(
+          (resultType) => resultType.type !== "topic"
+        );
       },
     });
   },
